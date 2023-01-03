2f0408e128372c4b471eacc7479dbd796828eba4
MikhaelVinicius <mickaelsoel@gmail.com>
2023-01-03 06:13:41 -0300

    Começo da implementação com Board e Piece

--- src/aplicacao/programa.java
+++ src/aplicacao/programa.java
@@ -1,9 +1,13 @@
 package aplicacao;
 
+import tabuleiro.Position;
+
 public class programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 
 	}
 
