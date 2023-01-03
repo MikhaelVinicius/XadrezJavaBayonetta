f51719261e302d0e7447f7896484eb6336150f75
MikhaelVinicius <mickaelsoel@gmail.com>
2023-01-03 08:04:09 -0300

    Camadas da tela e impressão do tabuleiro

--- src/aplicacao/programa.java
+++ src/aplicacao/programa.java
@@ -1,14 +1,14 @@
 package aplicacao;
 
-import tabuleiro.Position;
+import chess.ChessMatch;
 
 public class programa {
 
 	public static void main(String[] args) {
 	
-		Position pos = new Position(3, 5);
-		System.out.println(pos);
-
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
+		
 	}
 
 }
--- src/tabuleiro/Piece.java
+++ src/tabuleiro/Piece.java
@@ -2,4 +2,22 @@ package tabuleiro;
 
 public class Piece {
 
+	
+	protected Position position;
+	private Board board;
+
+	public Piece(Board board) {
+		
+		this.board = board;
+		position = null;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+
+
+	
+	
+	
 }
