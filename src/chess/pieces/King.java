fea9eefa5b52f9a00579e2df3b2608a353519819
MikhaelVinicius <mickaelsoel@gmail.com>
2023-01-06 18:11:25 -0300

    Movimentação do rei implementada

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,5 +1,6 @@
 package chess.pieces;
 import tabuleiro.Board;
+import tabuleiro.Position;
 import chess.ChessPiece;
 import chess.Cor;
 
@@ -15,11 +16,63 @@ public class King extends ChessPiece {
 		return "K";
 		
 	}
+	
+	private boolean canMove(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p == null || p.getCor() != getCor();
+ 		
+	}
+	
 
 
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getLinhas()][getBoard().getColunas()];
+		Position p = new Position(0, 0);
+		
+		p.setValues(position.getLinha()-1,position.getColuna());
+		if(getBoard().positionExists(p)&& canMove(p)) {
+			mat[p.getLinha()][p.getColuna()]=true;
+			}
+		
+		p.setValues(position.getLinha()+1,position.getColuna());
+		if(getBoard().positionExists(p)&& canMove(p)) {
+			mat[p.getLinha()][p.getColuna()]=true;
+			}
+		
+		p.setValues(position.getLinha(),position.getColuna()-1);
+		if(getBoard().positionExists(p)&& canMove(p)) {
+			mat[p.getLinha()][p.getColuna()]=true;
+			}
+		
+		p.setValues(position.getLinha(),position.getColuna()+1);
+		if(getBoard().positionExists(p)&& canMove(p)) {
+			mat[p.getLinha()][p.getColuna()]=true;
+			}
+		
+		p.setValues(position.getLinha()-1,position.getColuna()-1);
+		if(getBoard().positionExists(p)&& canMove(p)) {
+			mat[p.getLinha()][p.getColuna()]=true;
+			}
+		
+		p.setValues(position.getLinha()-1,position.getColuna()+1);
+		if(getBoard().positionExists(p)&& canMove(p)) {
+			mat[p.getLinha()][p.getColuna()]=true;
+			}
+		
+		p.setValues(position.getLinha()+1,position.getColuna()-1);
+		if(getBoard().positionExists(p)&& canMove(p)) {
+			mat[p.getLinha()][p.getColuna()]=true;
+			}
+		
+		p.setValues(position.getLinha()+1,position.getColuna()+1);
+		if(getBoard().positionExists(p)&& canMove(p)) {
+			mat[p.getLinha()][p.getColuna()]=true;
+			}
+		
+		
+		
+		
 		return mat;
 	}
 }
